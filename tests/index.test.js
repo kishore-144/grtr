01f47456b0670611b36350bbb38d7077be019b64
TheRootDaemon <harshath3018@gmail.com>
2025-11-13 23:10:55 +0530

    chore: Bump deps, applied formatting, resolved npm warnings

--- tests/index.test.js
+++ tests/index.test.js
@@ -38,7 +38,7 @@ describe("index.js CLI help", () => {
         process.argv = ["/usr/bin/node", "/mock/index.js"];
         const consoleSpy = vi
             .spyOn(console, "log")
-            .mockImplementation(() => { });
+            .mockImplementation(() => {});
 
         await import("../src/index.js");
 
@@ -54,7 +54,7 @@ describe("index.js CLI help", () => {
         process.argv = ["/usr/bin/node", "/mock/yada yada"];
         const consoleSpy = vi
             .spyOn(console, "log")
-            .mockImplementation(() => { });
+            .mockImplementation(() => {});
 
         await import("../src/index.js");
 
